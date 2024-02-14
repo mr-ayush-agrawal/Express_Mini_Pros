b802992b5c76b0855f65dde8c0036e80c0984aa6
Ayush <me21b1076@iiitdm.ac.in>
2024-02-14 22:58:55 +0530

    P1 - Making contact access Private

--- Contact_Manager/controller/contactController.js
+++ Contact_Manager/controller/contactController.js
@@ -5,19 +5,19 @@ const Contact = require('../Models/contactModel')
 
 // @desc GET all contacts
 // @route GET /api/contacts
-// @access public
+// @access private
 const getContacts = asyncHandler(async (req, res) => {
-    const contacts = await Contact.find();
+    const contacts = await Contact.find({ user_id: req.user.id });
     res.status(200).json(contacts)
 })
 
 // @desc Create New contacts
 // @route POST /api/contacts
-// @access public
-const createContact = asyncHandler(async(req, res) => {
+// @access private
+const createContact = asyncHandler(async (req, res) => {
     console.log(req.body)
-    const {name, phone, email} = req.body;
-    if(!name || !phone){
+    const { name, phone, email } = req.body;
+    if (!name || !phone) {
         res.status(400);
         throw new Error('Name and Phone number are mendatory');
     }
@@ -25,7 +25,8 @@ const createContact = asyncHandler(async(req, res) => {
     const contact = await Contact.create({
         name,
         phone,
-        email
+        email,
+        user_id : req.user.id
     })
 
     res.status(200).json(contact)
@@ -36,10 +37,10 @@ const createContact = asyncHandler(async(req, res) => {
 
 // @desc GET contact via id
 // @route GET /api/contacts/:id
-// @access public
-const getContact = asyncHandler(async(req, res) => {
-    const contact =await Contact.findById(req.params.id)
-    if(!contact){
+// @access private
+const getContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id)
+    if (!contact) {
         res.status(404);
         throw new Error('Contact Not found')
     }
@@ -51,18 +52,23 @@ const getContact = asyncHandler(async(req, res) => {
 
 // @desc Update contacts
 // @route PUT /api/contacts
-// @access public
-const updateContact = asyncHandler(async(req, res) => {
-    const contact =await Contact.findById(req.params.id)
-    if(!contact){
+// @access private
+const updateContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id)
+    if (!contact) {
         res.status(404);
         throw new Error('Contact Not found')
+    }
+
+    if(contact.user_id.toString() !== req.user.id){
+        res.status(403)
+        throw new Error("User don't have permision to update other user contact")
     }
 
     const updatedContact = await Contact.findByIdAndUpdate(
         req.params.id,
         req.body,
-        {new:true}
+        { new: true }
     )
 
     res.status(200).json(updatedContact)
@@ -72,17 +78,21 @@ const updateContact = asyncHandler(async(req, res) => {
 
 // @desc Update contacts
 // @route PUT /api/contacts
-// @access public
-const delContact = asyncHandler(async(req, res) => {
-    const contact =await Contact.findById(req.params.id)
-    if(!contact){
+// @access private
+const delContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id)
+    if (!contact) {
         res.status(404);
         throw new Error('Contact Not found')
     }
-    console.log(contact);
+    
+    if(contact.user_id.toString() !== req.user.id){
+        res.status(403)
+        throw new Error("User don't have permision to delete other user contact")
+    }
 
-    await Contact.findByIdAndDelete(req.params.id);
-    res.status(200).json(contact)   
+    await Contact.deleteOne({id: req.params.id});
+    res.status(200).json(contact)
 })
 
 
--- Contact_Manager/routes/contactRoutes.js
+++ Contact_Manager/routes/contactRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const {getContact,createContact, getContacts, updateContact, delContact} = require('../controller/contactController')
+const {getContact,createContact, getContacts, updateContact, delContact} = require('../controller/contactController');
+const validate = require('../MiddleWare/tokenValidator');
+
+router.use(validate)
 
 router.route('/').get(getContacts).post(createContact);
 router.route('/:id').get(getContact).put(updateContact).delete(delContact);
