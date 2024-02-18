0c84c2f61fc853805b839af807d9ccf87ed0ab26
Ayush <me21b1076@iiitdm.ac.in>
2024-02-18 09:59:43 +0530

    P3 - DataBse Connection

--- 3_StoteAPI/server.js
+++ 3_StoteAPI/server.js
@@ -1,21 +1,23 @@
-// require('dotenv').config();
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 5001;
 
 const errorHandler = require('./MiddleWare/errorHandler')
 const notFound = require('./MiddleWare/404_Notfound')
+const connectDB = require('./config/dbConnection')
+
 
 // using the middle wares
 app.use(express.json())
 app.use(errorHandler)
 
-// app.get('/', (req, res)=> res.send('Chsdgs  alu h'))
 app.get('/', (req, res)=> res.send('<h1>Store API</h1><a href="/products">Products</a>'))
 app.use(notFound)
 
 const start = async() => {
     try{
+        connectDB();
         app.listen(PORT, ()=> console.log(`Listning to PORT : ${PORT}`))
     }
     catch(err){
@@ -23,4 +25,4 @@ const start = async() => {
     }
 }
 
-start()
+start() 
