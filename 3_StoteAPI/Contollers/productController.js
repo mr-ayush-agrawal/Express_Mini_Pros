8b8fcca58bd1675c2c2c763c158a4b26d1770c38
Ayush <me21b1076@iiitdm.ac.in>
2024-02-19 01:42:06 +0530

    P3 - Request Parameters for Search

--- 3_StoteAPI/Contollers/productController.js
+++ 3_StoteAPI/Contollers/productController.js
@@ -1,13 +1,45 @@
+const Product = require('../Model/productModel')
 
+const getAllProStaic = async (req, res) => {
+    const search = 'ab'
+    const products = await Product.find({}).sort('company -price')
 
-const getAllPro = async(req,  res) => {
-    res.status(403)
-    throw new Error('Testing Async Err')
-    res.status(200).json({msg:'Testing message '})
+    res.status(200).json({ products, nbHits: products.length })
 }
 
-const getAllProStaic = async(req,  res) => {
-    res.status(200).json({msg:'Testing message Static'})
+const getAllPro = async (req, res) => {
+
+    const { featured, company, name, sort, feild } = req.query
+    const queryObject = {}
+
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false;
+    }
+    if (company) {
+        queryObject.company = company;
+    }
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i' };
+    }
+
+    let result = Product.find(queryObject)
+    if (sort) {
+        const sList = sort.split(',').join(' ')
+        console.log(sList)
+        result.sort(sList)
+    }
+    else {
+        result = result.sort('createdAt')
+    }
+
+    if (feild) {
+        const feildList = feild.split(',').join(' ')
+        result = result.select(feildList)
+    }
+
+    
+    const products = await result
+    res.status(200).json({ products, nbHits: products.length })
 }
 
-module.exports = {getAllPro, getAllProStaic}
+module.exports = { getAllPro, getAllProStaic }
