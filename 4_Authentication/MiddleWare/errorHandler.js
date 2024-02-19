8f65fa64ee6a6fc30d6d157a10514f66cb541828
Ayush <me21b1076@iiitdm.ac.in>
2024-02-19 19:40:47 +0530

    P4 - DataBase Connection

---
+++ 4_Authentication/MiddleWare/errorHandler.js
@@ -0,0 +1,40 @@
+const constants = require('../../Constants.js')
+
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode ? res.statusCode : 500;
+    switch (statusCode) {
+        case constants.VALIDAION_ERROR:
+            res.json({ 
+                title: "Validation Failed", 
+                message: err.message, 
+                stackTrack: err.stack 
+            });
+            break;
+
+        case constants.NOT_FOUND:
+            res.json({ 
+                title: "Not Found", 
+                message: err.message, 
+                stackTrack: err.stack 
+            });
+            break;
+        case constants.FORBIDDEN:
+            res.json({ 
+                title: "Forbidden", 
+                message: err.message, 
+                stackTrack: err.stack 
+            });
+            break;
+        case constants.UNAUTHORIZED:
+            res.json({ 
+                title: "Un authorized access", 
+                message: err.message, 
+                stackTrack: err.stack 
+            });
+            break;
+        default: console.log("No Error")
+    }
+    next();
+}
+
+module.exports = errorHandler
--- 4_Authentication/server.js
+++ 4_Authentication/server.js
@@ -1,14 +1,27 @@
-require('dotenv')
+require('dotenv').config()
+require('express-async-errors')
 const express = require('express')
+const errorHandler = require('./MiddleWare/errorHandler')
+const DBconnection = require('./config/connectDB')
+
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
 app.use(express.static('./public'))
+app.use(express.json())
 
-async function start () {
-    app.listen(PORT , ()=> console.log(`Listning to port : ${PORT}`))
+app.use(errorHandler)
+
+async function start() {
+    try {
+        DBconnection();
+        app.listen(PORT, () => console.log(`Listning to PORT : ${PORT}`))
+    }
+    catch (err) {
+        console.log(err)
+    }
 }
 
 start()
